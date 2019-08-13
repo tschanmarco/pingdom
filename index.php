6735bac2f125ade58f39dcc6e0b7fa09909d1c65
tschanmarco <4919403+tschanmarco@users.noreply.github.com>
2019-08-13 12:05:38 +0200

    Update index.php
    Transaction name styling

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	
+
     <!-- Einbinden des Bootstrap-Stylesheets -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
@@ -23,16 +23,16 @@
     <script src="js/bootstrap.min.js"></script>
 
     <style type="text/css">
-		body, a { font-family: Courier New; }
-		h1 { font: 18px "Courier New"; }
-		a { font-family: Arial, Verdana; }
-    .trans { font-family: Arial, Verdana; font-weight: bold; color:red; }
+      body, a { font-family: Courier New; }
+      h1 { font: 18px "Courier New"; }
+      a { font-family: Arial, Verdana; }
+      .trans { font-family: Arial, Verdana; font-weight: bold; color:#666; }
     </style>	
   </head>
 
   <body>
-	<section class="container">
-	<?php
+  <section class="container">
+  <?php
   
   $superuserkey = 543210;
   $iconsize = 22;
@@ -40,31 +40,31 @@
   $dashboard = false;
   $transfailfile = "transaction_failures.json";
 
-	// pingdom api request - checks
-	$ch = curl_init();	
-	$fp = fopen('errorlog.txt', 'w');
-	curl_setopt($ch, CURLOPT_URL, "https://api.pingdom.com/api/2.1/checks");
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-	curl_setopt($ch, CURLOPT_USERPWD, "support.migros-ch@mgb.ch:m96&m1ts");
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array("app-key: z1zygypqxr5ikzt23wug0cnoopaqqaok"));
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_VERBOSE, 1);
-	curl_setopt($ch, CURLOPT_STDERR, $fp);
+  // pingdom api request - checks
+  $ch = curl_init();	
+  $fp = fopen('errorlog.txt', 'w');
+  curl_setopt($ch, CURLOPT_URL, "https://api.pingdom.com/api/2.1/checks");
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  curl_setopt($ch, CURLOPT_USERPWD, "support.migros-ch@mgb.ch:m96&m1ts");
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array("app-key: z1zygypqxr5ikzt23wug0cnoopaqqaok"));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_VERBOSE, 1);
+  curl_setopt($ch, CURLOPT_STDERR, $fp);
 
-	// pingdom api request - shared for web
-	$ch2 = curl_init();	
-	$fp2 = fopen('errorlog.txt', 'w');
-	curl_setopt($ch2, CURLOPT_URL, "https://api.pingdom.com/api/2.1/reports.public");
-	curl_setopt($ch2, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch2, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-	curl_setopt($ch2, CURLOPT_USERPWD, "support.migros-ch@mgb.ch:m96&m1ts");
-	curl_setopt($ch2, CURLOPT_HTTPHEADER, array("app-key: z1zygypqxr5ikzt23wug0cnoopaqqaok"));
-	curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch2, CURLOPT_VERBOSE, 1);
-	curl_setopt($ch2, CURLOPT_STDERR, $fp2);
+  // pingdom api request - shared for web
+  $ch2 = curl_init();	
+  $fp2 = fopen('errorlog.txt', 'w');
+  curl_setopt($ch2, CURLOPT_URL, "https://api.pingdom.com/api/2.1/reports.public");
+  curl_setopt($ch2, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch2, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  curl_setopt($ch2, CURLOPT_USERPWD, "support.migros-ch@mgb.ch:m96&m1ts");
+  curl_setopt($ch2, CURLOPT_HTTPHEADER, array("app-key: z1zygypqxr5ikzt23wug0cnoopaqqaok"));
+  curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch2, CURLOPT_VERBOSE, 1);
+  curl_setopt($ch2, CURLOPT_STDERR, $fp2);
   
   // pingdom api request - transaction checks
   $ch3 = curl_init();	
@@ -79,14 +79,14 @@
   curl_setopt($ch3, CURLOPT_VERBOSE, 1);
   curl_setopt($ch3, CURLOPT_STDERR, $fp3);
 
-	if(isset($_GET['check'])) {
-		$showCheck = explode(',', $_GET['check']);
-	} else {
-		$showCheck = 0;
-	}
+  if(isset($_GET['check'])) {
+    $showCheck = explode(',', $_GET['check']);
+  } else {
+    $showCheck = 0;
+  }
   
   $dashboard = false;
-	if(isset($_GET['dashboard'])) {
+  if(isset($_GET['dashboard'])) {
     $iconsize = 55;
     $dashboard = true;
 ?>
@@ -98,27 +98,27 @@
 <?php
   }
 
-	$curl_response = curl_exec($ch);
-	curl_close($ch);
-	$arrChecks = json_decode($curl_response);
+  $curl_response = curl_exec($ch);
+  curl_close($ch);
+  $arrChecks = json_decode($curl_response);
 
-	$curl_response2 = curl_exec($ch2);
-	curl_close($ch2);
-	$arrShared = json_decode($curl_response2);
+  $curl_response2 = curl_exec($ch2);
+  curl_close($ch2);
+  $arrShared = json_decode($curl_response2);
   
   $curl_response3 = curl_exec($ch3);
-	curl_close($ch3);
-	$arrTrans = json_decode($curl_response3);
+  curl_close($ch3);
+  $arrTrans = json_decode($curl_response3);
   //print_r($arrTrans); exit;
 
-	// list of public checks
-	$arrPublicId = array();
-	foreach ($arrShared->public as $shared) {
-		$arrPublicId[] = $shared->checkid;
-	}
+  // list of public checks
+  $arrPublicId = array();
+  foreach ($arrShared->public as $shared) {
+    $arrPublicId[] = $shared->checkid;
+  }
 
-	// debug informations
-	if(($showCheck != 0 && in_array($superuserkey, $showCheck)) && isset($_GET['debug'])) {
+  // debug informations
+  if(($showCheck != 0 && in_array($superuserkey, $showCheck)) && isset($_GET['debug'])) {
     if($_GET['debug'] == "deepdive") {
       foreach ($arrChecks->checks as $check) {
         // pingdom api request - get check details
@@ -148,41 +148,33 @@
       echo "</pre>";
     }
     exit;
-	}
-
-	// header output
-	echo "<h1><nobr>Monitoring Digital Operation</nobr></h1>\n";
-	echo "<p>Refresh alle 90 Sekunden</p>\n";
-  
-  // category links
-  if($showCheck != 0 && in_array(superuserkey, $showCheck)) {
-    echo '<a href="http://media.migros.ch/monitoring/?check=2129204,790608,1922885,1922888,1526957,1922886,1482395,972027,1689232,1979788,1957207">migros.ch</a> | ';
-    echo '<a href="http://media.migros.ch/monitoring/?check=972260,1602331,3758203,2265019,3758176,3758089,3758149,2268735,2271851,3758377,3758404,3758428,3860014">M-API</a> | ';
-    echo '<a href="http://media.migros.ch/monitoring/?check=3758377,3758404,3388594,3388813,848099,3758089,3758149,3758404,2268735,2271851,3860014,3388516,3388543">DWH-WS</a> | ';	  
-	  echo '<a href="http://media.migros.ch/monitoring/?check=3388516,3388543,3388594,3388639,3325018,3388657,3388813,3388762,3388648,3388729">Testsysteme</a>';
-    echo "<br/><br/>";
   }
+
+  // header output
+  echo "<h1><nobr>Monitoring Digital Operation</nobr></h1>\n";
+  echo "<p>Refresh alle 90 Sekunden</p>\n";
+  
   echo "<table id='dashboard' class='table table-striped table-bordered table-sm'>";
-	echo "<thead><tr>";
+  echo "<thead><tr>";
   echo "<th>Up</th>";
   echo "<th>Checkname</th>";
   echo "<th>Hostname</th>";
   echo "<th><nobr>letzter Ausfall</nobr></th>";
   if (!$dashboard) echo "<th>ID</th>";
   echo "</tr></thead>\n";
-	echo "<tbody>\n";
+  echo "<tbody>\n";
 
-	// check sort
-	usort($arrChecks->checks, "my_cmp");
-	function my_cmp($a, $b) {
-	  if ($a->name == $b->name) {
-		return 0;
-	  }
-	  return (strtolower($a->name) < strtolower($b->name)) ? -1 : 1;
-	}
+  // check sort
+  usort($arrChecks->checks, "my_cmp");
+  function my_cmp($a, $b) {
+    if ($a->name == $b->name) {
+    return 0;
+    }
+    return (strtolower($a->name) < strtolower($b->name)) ? -1 : 1;
+  }
 
-	// stati
-	$arrStati = array("down","up");
+  // stati
+  $arrStati = array("down","up");
   
   // transaction cheks output
   foreach ($arrTrans->recipes as $key => $transcheck) {
@@ -196,7 +188,7 @@
         if($showCheck != 0 && (in_array($key, $showCheck) || in_array($superuserkey, $showCheck))) {
           echo "<tr>";
           echo "<td><img src='./down.png' width='$iconsize' height='$iconsize' title='FAILING'></td>";
-          echo "<td><a><b>".$transcheck->name."</b></a></td>";
+          echo "<td class='trans'>".$transcheck->name."</td>";
           echo "<td>TRANSACTION</td>";
           echo "<td style='font-weight:bold; color:red; white-space:nowrap'>".date('Y-m-d, H:i:s')."</td>";
           if (!$dashboard) echo "<td>".$key."</td>";
@@ -212,7 +204,7 @@
         if($showCheck != 0 && (in_array($key, $showCheck) || in_array($superuserkey, $showCheck))) {
           echo "<tr>";
           echo "<td><img src='./up.png' width='$iconsize' height='$iconsize' title='SUCCESSFUL'></td>";
-          echo "<td><a><b>".$transcheck->name."</b></a></td>";
+          echo "<td class='trans'>".$transcheck->name."</td>";
           echo "<td>TRANSACTION</td>";
           echo "<td " . $lasterrorStyle . ">".$arrTransFailure[$key]."</td>";
           if (!$dashboard) echo "<td>".$key."</td>";
@@ -224,7 +216,7 @@
   
   // print_r($arrChecks);exit;
   
-	// checks output
+  // checks output
   foreach ($arrChecks->checks as $check) {
         
       // status image
@@ -259,10 +251,10 @@
         echo "</tr>\n";
       }
     }
-	echo "</tbody>\n";
-	echo "</table>\n";
-	?>
-	</section>
+  echo "</tbody>\n";
+  echo "</table>\n";
+  ?>
+  </section>
   </body>
 
 </html>
@@ -270,8 +262,8 @@
 <script>
 $(document).ready(function() {
     $('#dashboard').dataTable( {
-		"paging": false, 
-		"order": [[ 3, 'desc' ]]
-	} );
+      "paging": false, 
+      "order": [[ 3, 'desc' ]]
+    } );
 } );
 </script>
